d60e4d207b73cacc71fc7b9e706c942807ecb48f
jhonflo296-pixel <jhonflo296@gmail.com>
2026-09-21 01:44:56 -0500

    Add server.js for Google Cloud deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-let PORT = 3000;
+let PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 const PUBLIC_DIR = __dirname;
 
 const MIME_TYPES = {
